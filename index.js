1262eb518acea7ec3762752ca4c613c64b56a46b
Greg Bernstein <gregb@grotto-networking.com>
2019-08-14 11:51:08 -0700

    Function Component Example 2

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import questions from './questions.json';
 
-// Function Component Definition
-function Welcome(props) { // Start with Capital
-    return <h1> Hello, {props.name} </h1>; // returns JSX
+function randInt(max){
+  return Math.floor(Math.random()* Math.floor(max));
 }
-// Function component use -- looks like custom HTML
-let contents = <section>
-        <Welcome name="Web Development" />
-        <Welcome name="Dr. B" />
-        </section>;
 
-ReactDOM.render(contents,
-    document.getElementById('root')
+function Choices(props) {
+    // Creates an array of list item JSX elements with key
+	let listItems = props.choices.map(function(choice, i) {
+        return <li key={"choice" + i}>{choice}</li>;});
+    // Set some styling on the list note that
+    // list-style-type becomes listStyleType
+    let choiceStyle = {listStyleType: "lower-alpha"};
+    return <ol style={choiceStyle}>
+        {listItems}
+        </ol>;
+}
+
+function Question(props) {
+    return <div>
+        <p>{props.multiChoiceQ.question}</p>
+        <Choices choices={props.multiChoiceQ.choices} />
+    </div>;
+}
+
+let mChoice = questions[randInt(questions.length)];
+
+let contents = <div><h1>Quiz-o-Matic</h1>
+        <Question multiChoiceQ={mChoice} />
+    </div>;
+
+ReactDOM.render(
+    contents,
+  document.getElementById('root')
 );
 
 
