89ec4b86a706e06115752ccbd499b02fa6e31d65
Greg Bernstein <gregb@grotto-networking.com>
2019-08-14 11:43:41 -0700

    JSX 4 example.

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import members from './students1.json'; // Practice data
+import chemElements from './elements.json'; // Practice data
 
-const intro = <h1>Our Coding Club!</h1>;
-// Take a look at what we imported
-console.log(members);
-// Extremely common pattern mapping array of info to array of JSX
-let rows = members.map(function(u){
-  return <tr key={u.netid}><td>{u.netid}</td><td>{u.firstName}</td><td>{u.lastName}</td></tr>;
+function randInt(max){
+  return Math.floor(Math.random()* Math.floor(max));
+}
+
+let num = randInt(100);
+let range = 10;
+const intro = <h2>Element with Atomic number {num} through {num + range}</h2>;
+
+// Filter only the elements we want
+let filtered = chemElements.filter(function(u){
+  if (u.atomic_number >= num && u.atomic_number <= num + range )
+    return true;
+  else
+    return false;
+});
+// CSS styles to be applied to elements
+let liStyle = {display: "flex", justifyContent: "space-between",
+  fontFamily: "sans-serif"};
+let numStyle = {backgroundColor: "yellow", color: "blue", width: "10em"};
+let nameStyle = {backgroundColor: "gray", color: "white", width: "10em"};
+// Map pattern
+let listItems = filtered.map(function(u){
+  return <li key={u.symbol} style={liStyle}>
+    <span style={numStyle}>Number: {u.atomic_number}, </span>
+    <span style={nameStyle}>Name: {u.name}, </span>
+    <span style={numStyle}>Symbol: {u.symbol},</span>
+    <span style={nameStyle}>Atomic weight: {u.atomic_weight}</span></li>;
 });
 
-let memberTable = <table className="myTable">
-  <thead><tr><th>NetId</th><th>First</th><th>Last</th></tr></thead>
-  <tbody>{rows}</tbody>
-</table>;
+let elementList = <ul>
+  {listItems}
+</ul>;
 
-ReactDOM.render(<section>{intro}{memberTable}</section>,
+ReactDOM.render(<section>{intro}{elementList}</section>,
   document.getElementById('root'));
 
 
