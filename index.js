d2ddd683316f0eefe16825fdd791b8bf2af2720a
Greg Bernstein <gregb@grotto-networking.com>
2019-08-14 11:46:30 -0700

    Function Component Example 1

--- index.js
+++ index.js
@@ -1,42 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import chemElements from './elements.json'; // Practice data
 
-function randInt(max){
-  return Math.floor(Math.random()* Math.floor(max));
+// Function Component Definition
+function Welcome(props) { // Start with Capital
+    return <h1> Hello, {props.name} </h1>; // returns JSX
 }
+// Function component use -- looks like custom HTML
+let contents = <section>
+        <Welcome name="Web Development" />
+        <Welcome name="Dr. B" />
+        </section>;
 
-let num = randInt(100);
-let range = 10;
-const intro = <h2>Element with Atomic number {num} through {num + range}</h2>;
-
-// Filter only the elements we want
-let filtered = chemElements.filter(function(u){
-  if (u.atomic_number >= num && u.atomic_number <= num + range )
-    return true;
-  else
-    return false;
-});
-// CSS styles to be applied to elements
-let liStyle = {display: "flex", justifyContent: "space-between",
-  fontFamily: "sans-serif"};
-let numStyle = {backgroundColor: "yellow", color: "blue", width: "10em"};
-let nameStyle = {backgroundColor: "gray", color: "white", width: "10em"};
-// Map pattern
-let listItems = filtered.map(function(u){
-  return <li key={u.symbol} style={liStyle}>
-    <span style={numStyle}>Number: {u.atomic_number}, </span>
-    <span style={nameStyle}>Name: {u.name}, </span>
-    <span style={numStyle}>Symbol: {u.symbol},</span>
-    <span style={nameStyle}>Atomic weight: {u.atomic_weight}</span></li>;
-});
-
-let elementList = <ul>
-  {listItems}
-</ul>;
-
-ReactDOM.render(<section>{intro}{elementList}</section>,
-  document.getElementById('root'));
+ReactDOM.render(contents,
+    document.getElementById('root')
+);
 
 
