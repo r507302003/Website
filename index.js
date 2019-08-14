d8fc1b141525fba6aa8f2e991570e307ada63e5f
Greg Bernstein <gregb@grotto-networking.com>
2019-08-14 11:28:50 -0700

    The first JSX example.

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-// What is this? HTML mixed with JavaScript
-ReactDOM.render(
-	<h1> Hello Website Development! </h1>,
-	document.getElementById("root")
-);
+const intro = <h1>Hello Website Development!</h1>;
+function windSpeed() {
+      return 25*Math.random();
+}
+
+let mySail = 6.6;
+let report = <div><p>Sailing on my {mySail}.</p>
+              <p>The windspeed is {windSpeed().toFixed(1)}.</p>
+              </div>;
+
+ReactDOM.render(<section>
+  {intro}
+  {report}
+  </section>,
+  document.getElementById('root'));
+
+
