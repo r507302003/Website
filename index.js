834920489d9b59dd45997910efe420bbed09eeaa
Greg Bernstein <gregb@grotto-networking.com>
2019-08-14 11:32:16 -0700

    JSX 1.1 example.

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const intro = <h1>Hello Website Development!</h1>;
 function windSpeed() {
       return 25*Math.random();
 }
 
-let mySail = 6.6;
-let report = <div><p>Sailing on my {mySail}.</p>
-              <p>The windspeed is {windSpeed().toFixed(1)}.</p>
+let intro = <h1>Hello Website Development!</h1>;
+let wind = windSpeed();
+if (wind > 12) {
+  intro = <h1>Go Sailing!</h1>
+}
+let report = <div>
+              <p>The windspeed is {wind.toFixed(1)}.</p>
               </div>;
 
 ReactDOM.render(<section>
