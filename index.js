f20415e7ab3cdf5ab37daa8ebf21a88f5b4c7237
Greg Bernstein <gregb@grotto-networking.com>
2019-08-14 11:20:00 -0700

    A bit of clean up on formatting using Prettier.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // What is this? HTML mixed with JavaScript
-ReactDOM.render(<h1>Hello Website Development!</h1>,
-  document.getElementById('root'));
+ReactDOM.render(
+	<h1> Hello Website Development! </h1>,
+	document.getElementById("root")
+);
