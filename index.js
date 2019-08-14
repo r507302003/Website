b992a328d9f8c6a315b91d329d5b8d333e309b84
Greg Bernstein <gregb@grotto-networking.com>
2019-08-14 11:16:36 -0700

    Initial commit of Parcel based React development setup.

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// What is this? HTML mixed with JavaScript
+ReactDOM.render(<h1>Hello Website Development!</h1>,
+  document.getElementById('root'));
